d158a346a46c1899792290d5495b13041b00d290
alvaroolanda14-esp <AlvaroOlanda14@gmail.com>
2026-04-18 06:53:05 -0500

    build: actualizando la carpeta de archivos compilados estaticos para Railway

--- Frontend/browser/chunk-HGLM4GPU.js
+++
@@ -1,1 +0,0 @@
-import{Ra as a}from"./chunk-33VLNZVH.js";export{a as GestionarDisponibilidadComponent};
---
+++ Frontend/browser/chunk-LOXGKBSL.js
@@ -0,0 +1,1 @@
+import{g as a}from"./chunk-3KDKQF2B.js";import"./chunk-WZTUAPJB.js";export{a as GestionarDisponibilidadComponent};

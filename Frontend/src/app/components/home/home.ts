3303fe172a6898622a800ff4686ef2716da84873
alvaroolanda14-esp <AlvaroOlanda14@gmail.com>
2026-05-24 16:33:08 -0500

    feat: implement two-column layout for sedes map with interactive sidebar list

--- Frontend/src/app/components/home/home.ts
+++ Frontend/src/app/components/home/home.ts
@@ -25,11 +25,14 @@ export class HomeComponent implements AfterViewInit, OnInit {
   doctores: any[] = [];
 
   sedes = [
-    { nombre: 'Sede Miraflores', direccion: 'Av. Larco 123', icono: 'fa-hospital', lat: -12.1227, lng: -77.0296, img: 'assets/images/SedeMiraflores.jpg' },
-    { nombre: 'Sede La Molina', direccion: 'Av. Javier Prado 456', icono: 'fa-building', lat: -12.0722, lng: -76.9450, img: 'assets/images/SedeMolina.jpg' },
-    { nombre: 'Sede Surco', direccion: 'Av. Primavera 789', icono: 'fa-clinic-medical', lat: -12.1167, lng: -76.9833, img: 'assets/images/SedeSurco.jpg' },
-    { nombre: 'Sede Santa Anita', direccion: 'Av. Ruiseñores 120', icono: 'fa-house-medical', lat: -12.0433, lng: -76.9536, img: 'assets/images/SedeSantaAnita.jpg' }
+    { nombre: 'Sede Miraflores', direccion: 'Av. Larco 123', telefono: '01-555-1234', icono: 'fa-hospital', lat: -12.1227, lng: -77.0296, img: 'assets/images/SedeMiraflores.jpg' },
+    { nombre: 'Sede La Molina', direccion: 'Av. Javier Prado 456', telefono: '01-555-5678', icono: 'fa-building', lat: -12.0722, lng: -76.9450, img: 'assets/images/SedeMolina.jpg' },
+    { nombre: 'Sede Surco', direccion: 'Av. Primavera 789', telefono: '01-555-9012', icono: 'fa-clinic-medical', lat: -12.1167, lng: -76.9833, img: 'assets/images/SedeSurco.jpg' },
+    { nombre: 'Sede Santa Anita', direccion: 'Av. Ruiseñores 120', telefono: '01-555-3456', icono: 'fa-house-medical', lat: -12.0433, lng: -76.9536, img: 'assets/images/SedeSantaAnita.jpg' }
   ];
+
+  private map: any;
+  private markers: any[] = [];
 
   constructor(
     private router: Router,
@@ -76,14 +79,14 @@ export class HomeComponent implements AfterViewInit, OnInit {
 
   private initMap() {
 
-    const map = L.map('map', {
+    this.map = L.map('map', {
       scrollWheelZoom: false,
       attributionControl: false 
     }).setView([-12.095, -77.01], 13);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       maxZoom: 20
-    }).addTo(map);
+    }).addTo(this.map);
 
     this.sedes.forEach(sede => {
       const popupContent = `
@@ -114,7 +117,9 @@ export class HomeComponent implements AfterViewInit, OnInit {
         opacity: 1,
         fillOpacity: 0.9,
         className: 'pulse-marker' 
-      }).addTo(map);
+      }).addTo(this.map);
+
+      this.markers.push({ sede, marker: circle });
 
       circle.bindPopup(popupContent, {
         closeButton: false,
@@ -127,6 +132,20 @@ export class HomeComponent implements AfterViewInit, OnInit {
   @HostListener('window:agendar-cita-mapa', ['$event'])
   onAgendarCitaMapa(event: any) {
     this.sacarCita();
+  }
+
+  centrarMapa(sede: any) {
+    if (this.map) {
+      this.map.flyTo([sede.lat, sede.lng], 15, {
+        animate: true,
+        duration: 1.5
+      });
+      // Find marker and open popup
+      const markerObj = this.markers.find(m => m.sede.nombre === sede.nombre);
+      if (markerObj) {
+        setTimeout(() => markerObj.marker.openPopup(), 1500);
+      }
+    }
   }
 
   sacarCita() {
